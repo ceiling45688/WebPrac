35097a445813bfe7047c9d3cca0ea467ed87aa83
ceiling12138 <ceiling12138@aliyun.com>
2018-12-18 22:33:35 +0800

    哎西..

---
+++ ExamplePrac/Prac2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>How are you</title>
+</head>
+<body>
+<?php
+$string1 = $_GET["name"];
+$string2 = $_GET["love"];
+echo ",您喜欢的体育运动时：";
+echo $string2;
+?>
+</body>
+</html>
