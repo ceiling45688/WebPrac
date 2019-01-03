9b7590a3edd4fb06a729bc46b6bd691118529b39
ceiling12138 <ceiling12138@aliyun.com>
2019-01-03 22:45:30 +0800

    数据库连接失败失败失败~~~

--- ExamplePrac/9.2.php
+++ ExamplePrac/9.2.php
@@ -4,11 +4,20 @@
  * User: celing
  * Date: 2019/1/2
  * Time: 23:43
- */$link = mysqli_connect("localhost","root","balbabal用时再改");
+ */$link = mysqli_connect("localhost","root","2016212888");
  if (!@$link) {
      echo "连接失败！ <br>";
     die();
  }
- echo "连接成功！";
+ echo "连接成功！<br>";
+ @mysqli_select_db("test",$link);
+ if (@mysqli_errno()){
+     echo "数据库选择失败！<br>";
+     die();
+ }
+ echo "数据库选择成功！<br>";
+
+ mysqli_query($link,"CREATE TABLE mysql_php_prac(xh CHAR (9) NOT NULL ,xm CHAR(9))" or die("失败！"));
+ echo "success!";
 
  ?>
