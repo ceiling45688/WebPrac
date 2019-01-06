434e2d38ad685445a8c7f83f0027d3abae2b7218
ceiling12138 <ceiling12138@aliyun.com>
2019-01-06 22:36:56 +0800

    还有改查没有练。

---
+++ ExamplePrac/stu_insertdelete.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: celing
+ * Date: 2019/1/6
+ * Time: 21:39
+ */
+$link = mysqli_connect("localhost","root","123456");
+echo "connect successfully! <br>";
+mysqli_select_db($link,"student_db") or die("failed!");
+echo "select successfully! <br>";
+//---------------insert---------
+//$sql = "insert into test (xh,xm,xb) VALUES ('854','wang','male')";
+//echo "insert successfully!"
+//注意双引号里面单引号。
+//---------------delete---------
+$sql ="delete from test WHERE xh='854'";
+if (mysqli_query($link,$sql)){
+    echo "delete successfully! <br>";
+}else
+    echo "delete failed!";
+//??明明没有数据了为什么还是 query ok?
+//$row = mysqli_fetch_row($result);
+//if (!$row){
+//    echo "failed!";
+//    die();
+//}
+?>
