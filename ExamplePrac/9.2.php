d0a385479fe4aef15477c9f805acde2e5d2e91e9
ceiling12138 <ceiling12138@aliyun.com>
2019-01-14 23:39:01 +0800

    还有增删改查没有练。

--- ExamplePrac/9.2.php
+++ ExamplePrac/9.2.php
@@ -5,7 +5,7 @@
  * Date: 2019/1/2
  * Time: 23:43
  */
-$link = mysqli_connect("localhost","root","2016212888");
+$link = mysqli_connect("localhost","root","123456");
 if (!@$link) {
     echo "连接失败！ <br>";
     die();
@@ -15,7 +15,7 @@ echo "连接成功！<br>";
 mysqli_select_db($link,"student_db")or die("连接数据库失败！");
 echo "数据库选择成功！<br>";
 
-$sql  = "CREATE TABLE if NOT EXISTS test4 (xh CHAR (10) NOT NULL ,xm CHAR(10) NOT NULL)";
+$sql  = "CREATE TABLE if NOT EXISTS test (xh CHAR (10) NOT NULL ,xm CHAR(10) NOT NULL)";
 //不一定要分开拼接字符串，但是一定注意要写（）还有if not exists！
 //$sql .= "xm char(10) not null)";
 
